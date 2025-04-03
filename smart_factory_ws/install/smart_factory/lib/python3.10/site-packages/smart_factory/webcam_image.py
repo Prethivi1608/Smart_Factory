e13d805754a9c351d23a1ee35eadcd728f560003
Prethivi1608 <prethiviraj@gmail.com>
2025-04-03 16:56:55 +1100

    Added a new folder for Webcam Adapter Images

---
+++ smart_factory_ws/install/smart_factory/lib/python3.10/site-packages/smart_factory/webcam_image.py
@@ -0,0 +1,28 @@
+import cv2
+def captureImage():
+    camera_name = 2
+    vedioCapture = cv2.VideoCapture(camera_name)
+    
+    num_images = 500
+    file_path = "/home/prethivi/ros2_ws/Smart_Factory/smart_factory_ws/src/smart_factory/camera_webcam"
+    
+    for i in range(num_images):
+        success,frame = vedioCapture.read()
+    
+        if success == True:
+            file_name = f'{file_path}image_file{i}.jpg'
+            cv2.imwrite(file_name,frame)
+            cv2.waitKey(0)
+                
+    
+    vedioCapture.release()
+
+cv2.destroyAllWindows()
+
+
+def main():
+    captureImage()
+    
+    
+if __name__ == '__main__':
+    main()
