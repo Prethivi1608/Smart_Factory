0599390573dd2270b5718aa934adf91bb15e9d82
Prethivi1608 <prethiviraj@gmail.com>
2025-04-04 02:01:40 +1100

    Added Hardware Webcam Model

--- smart_factory_ws/src/smart_factory/smart_factory/yolomodel.py
+++ smart_factory_ws/src/smart_factory/smart_factory/yolomodel.py
@@ -3,6 +3,6 @@ from ultralytics import YOLO
 
 model = YOLO('yolo11n.pt')
 
-model.train(data='/home/prethivi/ros2_ws/Smart_Factory/smart_factory_ws/src/smart_factory/RS2-hardware-Model.v1i.yolov11/data.yaml',epochs=60,imgsz=640)
+model.train(data='/home/prethiviraj/ros2/workspaces/Smart_Factory/smart_factory_ws/src/smart_factory/RS2-hardware-Model.v1i.yolov11/data.yaml',epochs=60,imgsz=640)
 
-model.save('/home/prethiviraj/ros2/workspaces/Smart_Factory/smart_factory_ws/src/smart_factory/yolo_model/rs2-hardware.pt')
+model.save('/home/prethiviraj/ros2/workspaces/Smart_Factory/smart_factory_ws/src/smart_factory/yolo_model/rs2_hardware.pt')
