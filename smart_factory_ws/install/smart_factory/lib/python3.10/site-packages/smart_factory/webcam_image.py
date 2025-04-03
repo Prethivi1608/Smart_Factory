860e8f0fa08da4832b6558ea42aeb75473cb5109
Prethivi1608 <prethiviraj@gmail.com>
2025-04-04 02:01:40 +1100

    Added Hardware Webcam Model

--- smart_factory_ws/install/smart_factory/lib/python3.10/site-packages/smart_factory/webcam_image.py
+++ smart_factory_ws/install/smart_factory/lib/python3.10/site-packages/smart_factory/webcam_image.py
@@ -3,14 +3,14 @@ def captureImage():
     camera_name = 2
     vedioCapture = cv2.VideoCapture(camera_name)
     
-    num_images = 500
-    file_path = "/home/prethivi/ros2_ws/Smart_Factory/smart_factory_ws/src/smart_factory/camera_webcam"
+    num_images = 250
+    file_path = "/home/prethivi/ros2_ws/Smart_Factory/smart_factory_ws/src/smart_factory/camera_webcam/"
     
     for i in range(num_images):
         success,frame = vedioCapture.read()
     
         if success == True:
-            file_name = f'{file_path}image_file{i}.jpg'
+            file_name = f'{file_path}image_file{i+750}.jpg'
             cv2.imwrite(file_name,frame)
             cv2.waitKey(0)
                 
--- smart_factory_ws/src/smart_factory/smart_factory/image_classifier.py
+++ smart_factory_ws/src/smart_factory/smart_factory/image_classifier.py
@@ -1,60 +1,31 @@
-import cv2
 from cv_bridge import CvBridge
 import rclpy
 from rclpy.node import Node
-
 from sensor_msgs.msg import Image
-from geometry_msgs.msg import Twist
 from ultralytics import YOLO
-import os
 
 class ImageClassifier(Node):
     def __init__(self):
-        super().__init__('image_classifier')
+        super().__init__('image_subscriber')
         
-        yolo_file = '/home/prethivi/ros2_ws/Smart_Factory/smart_factory_ws/src/smart_factory/yolo_model/smart_fact.pt'
+        yolo_file = '/home/prethiviraj/ros2/workspaces/Smart_Factory/smart_factory_ws/src/smart_factory/yolo_model/rs2_hardware.pt'
         
-        self.image_topic = '/camera/image_raw'
-        self.image_pub_topic = '/camera/image_process'
-        self.image_subscriber = self.create_subscription(Image,self.image_topic,self.image_callback,10)
-        self.velocity_publisher = self.create_publisher(Twist,'/cmd_vel',10)
-        #self.timer = self.create_timer(0.5,self.move_robot)
+        self.image_topic = '/camera/image_process'
         self.bridge = CvBridge()
-        self.image_pub = self.create_publisher(Image,self.image_pub_topic,10)
+        self.image_pub = self.create_publisher(Image,self.image_topic,10)
+        self.image_subscriber = self.create_subscription(Image,self.image_topic,10)
+        self.timer = self.create_timer(0.5,self.image_callback)
         self.model = YOLO(yolo_file)
         
-    def image_callback(self,ImgMsg):
-        img_width = 640
-        img_height = 480
-        self.image = self.bridge.imgmsg_to_cv2(ImgMsg)
-        
-        
-        self.results=self.model.track(source=self.image)
+    def image_callback(self):
+
+        self.results=self.model.track(source=0)
         annotated_frame = self.results[0].plot()
 
-        # self.boxes = self.results[0].boxes.xywh
-
-        # for box in self.boxes:
-        #     x,y,w,h = box
-
-        #     print(f"x={x},y={y},width={w},height={h}")
-            
-        #     while w>50:
-        #         self.move_robot()
-
-        
-        
         self.pub_image = self.bridge.cv2_to_imgmsg(annotated_frame)
         
         self.image_pub.publish(self.pub_image)
     
-    def move_robot(self):
-        self.velocity = Twist()
-        self.velocity.linear.x = 0.1
-
-
-        self.velocity_publisher.publish(self.velocity)
-
 def main():
     rclpy.init()
     image_class = ImageClassifier()
