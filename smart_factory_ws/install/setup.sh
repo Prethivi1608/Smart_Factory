44c9c7d6e56248d2a5f619343f5d7bce2e9e498b
Prethivi1608 <prethiviraj@gmail.com>
2025-04-07 07:25:29 +1000

    Added functions in go_to_goal

--- smart_factory_ws/install/setup.sh
+++ smart_factory_ws/install/setup.sh
@@ -38,10 +38,6 @@ _colcon_prefix_chain_sh_source_script "$COLCON_CURRENT_PREFIX/local_setup.sh"
 COLCON_CURRENT_PREFIX="/home/prethiviraj/ros2/workspaces/turtlebot3/install"
 _colcon_prefix_chain_sh_source_script "$COLCON_CURRENT_PREFIX/local_setup.sh"
 
-# setting COLCON_CURRENT_PREFIX avoids relying on the build time prefix of the sourced script
-COLCON_CURRENT_PREFIX="/home/prethiviraj/ros2/workspaces/smart_fact_hw/install"
-_colcon_prefix_chain_sh_source_script "$COLCON_CURRENT_PREFIX/local_setup.sh"
-
 
 # source this prefix
 # setting COLCON_CURRENT_PREFIX avoids relying on the build time prefix of the sourced script
